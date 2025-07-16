373b599c38de80b155110df601bc55ab9a51313e
grkndev <gweepcreative@gmail.com>
2025-07-16 14:59:54 +0300

    f

--- app/(screens)/_layout.tsx
+++ app/(screens)/_layout.tsx
@@ -1,11 +1,9 @@
-import { Slot, Stack } from 'expo-router'
+import { Stack } from 'expo-router'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function _layout() {
   return <SafeAreaView className='flex-1'>
-    <Stack screenOptions={{ animation: "fade", headerShown: false }}>
-      <Slot />
-    </Stack>
+    <Stack screenOptions={{ animation: "fade", headerShown: false }} />
   </SafeAreaView>
 }
